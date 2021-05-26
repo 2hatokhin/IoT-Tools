d78351749c8c523bc9bb354f934a07273df10efc
Andrei.Shatokhin <andrei.shatokhin@nordigy.ru>
2021-05-26 14:03:53 +0300

    Created working client

--- mqtt-button/src/script.js
+++ mqtt-button/src/script.js
@@ -21,17 +21,43 @@ const createStatusCircle = () => {
  */
 const updateStatusCircle = (value) => {
   const statusCircle = document.getElementsByClassName("status-circle")[0];
-
-  value >= 50
-    ? statusCircle.classList.add("alert-circle")
-    : statusCircle.classList.remove("alert-circle");
-
+  if (value >= 50) {
+    value = "Alert!"
+  }
+  value == "Alert!"
+        ? statusCircle.classList.add("alert-circle")
+        : statusCircle.classList.remove("alert-circle");
   value < 50
       ? statusCircle.classList.add("regular-circle")
       : statusCircle.classList.remove("regular-circle");
   statusCircle.innerText = value;
+
 };
 
+
+var client = new Paho.MQTT.Client("192.168.0.102", 9001, "clientId");
+
+
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+
+client.connect({onSuccess:onConnect});
+
+function onConnect() {
+  console.log("Connected to host");
+  client.subscribe("house/bulb1");
+}
+
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+function onMessageArrived(message) {
+  console.log("Value:"+message.payloadString);
+  updateStatusCircle(message.payloadString);
+}
 createStatusCircle();
 
-updateStatusCircle(49);
